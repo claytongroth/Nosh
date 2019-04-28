840929b818911122c60698b81387508c9957cd4b
Andra L. Mathews <alarae@users.noreply.github.com>
2019-04-28 14:18:10 -0500

    Add files via upload
    Updated content re: Titles
    App.js --> h2 
    frontend.js --> changed to OFFDQ

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,97 @@ import React, { Component } from "react";
 import axios from "axios";
 import Frontend from "./frontend";
 
-class OldApp extends Component {
+class App extends Component {
+  // initialize our state
+  state = {
+    data: [],
+    id: 0,
+    message: null,
+    intervalIsSet: false,
+    idToDelete: null,
+    idToUpdate: null,
+    objectToUpdate: null
+  };
+
+  // when component mounts, first thing it does is fetch all existing data in our db
+  // then we incorporate a polling logic so that we can easily see if our db has
+  // changed and implement those changes into our UI
+  componentDidMount() {
+    this.getDataFromDb();
+    if (!this.state.intervalIsSet) {
+      let interval = setInterval(this.getDataFromDb, 1000000);
+      this.setState({ intervalIsSet: interval });
+    }
+  }
+
+  // never let a process live forever
+  // always kill a process everytime we are done using it
+  componentWillUnmount() {
+    if (this.state.intervalIsSet) {
+      clearInterval(this.state.intervalIsSet);
+      this.setState({ intervalIsSet: null });
+    }
+  }
+
+  // just a note, here, in the front end, we use the id key of our data object
+  // in order to identify which we want to Update or delete.
+  // for our back end, we use the object id assigned by MongoDB to modify
+  // data base entries
+
+  // our first get method that uses our backend api to
+  // fetch data from our data base
+  getDataFromDb = () => {
+    fetch("http://localhost:27017/api/getData" + "?id=0000000035590&brands=Taste%20Adventure") //  Query format here: fetch("http://localhost:27017/api/getData" + "?id=234234324342&brands=Taste%20Adventure")
+    //.then(data => data.text()).then(data => console.log(data))
+    .then(data => data.json())
+    .then(answer => this.setState({ data: answer }, console.log(this.state.data)));
+  };
+
+  // our put method that uses our backend api
+  // to create new query into our data base
+  putDataToDB = message => {
+    let currentIds = this.state.data.map(data => data.id);
+    let idToBeAdded = 0;
+    while (currentIds.includes(idToBeAdded)) {
+      ++idToBeAdded;
+    }
+    axios.post("http://localhost:27017/api/putData", {
+      id: idToBeAdded,
+      message: message
+    });
+  };
+  // our delete method that uses our backend api
+  // to remove existing database information
+  deleteFromDB = idTodelete => {
+    let objIdToDelete = null;
+    this.state.data.forEach(dat => {
+      if (dat.id == idTodelete) {
+        objIdToDelete = dat._id;
+      }
+    });
+    axios.delete("http://localhost:27017/api/deleteData", {
+      data: {
+        id: objIdToDelete
+      }
+    });
+  };
+  // our update method that uses our backend api
+  // to overwrite existing data base information
+  updateDB = (idToUpdate, updateToApply) => {
+    let objIdToUpdate = null;
+    this.state.data.forEach(dat => {
+      if (dat.id == idToUpdate) {
+        objIdToUpdate = dat._id;
+      }
+    });
+
+    axios.post("http://localhost:27017/api/updateData", {
+      id: objIdToUpdate,
+      update: { message: updateToApply }
+    });
+  };
+
+
   // here is our UI
   // it is easy to understand their functions when you
   // see them render into our screen
@@ -11,13 +101,18 @@ class OldApp extends Component {
 
   // there will be a button which will launch get data and set variables to query
   render() {
+    const db = this.state.data;
+    console.log("should render", db)
     return (
       <div className="col">
-        <h1>Nosh</h1>
+        <h1>Sustainable Consumerism: Food Product Decision-Making</h1>
+        <p>Find sustainable food products.</p>
+        {console.log(db.map(db => db.ingredients_text))}
+        {db.map(db => <div>{db.ingredients_text}</div>)}
         <Frontend/>
       </div>
     );
   }
 }
 
-export default OldApp;
+export default App;
--- client/src/frontend.js
+++ client/src/frontend.js
@@ -6,122 +6,7 @@ import './bootstrap.css';
 import './style.css'
 
 
-class MainApp extends React.Component {
-  constructor(props, context) {
-  super(props, context);
-    this.state = {
-      gtin: null,
-      mlocation: null,
-      ingredients: [],
-      labels: [],
-      data: [],
-      id: 0,
-      message: null,
-      intervalIsSet: false,
-      idToDelete: null,
-      idToUpdate: null,
-      objectToUpdate: null
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleSubmit(e){
-    e.preventDefault();
-    let mongoQuery = "?id=" + this.state.gtin.toString();
-    // create the mongo query in the logic here
-    // 0016229004019
-    this.getDataFromDb(mongoQuery);
-    console.log("submited: ", mongoQuery)
-  }
-  handleChange(e){
-    let field = e.target.name;
-    let value = e.target.value;
-    console.log(field);
-    if (field === "Universal Product Code"){this.setState({gtin: value}, console.log("State for ", field, " uppdated to: ", this.state.gtin));};
-    if (field === "Manufacturing Location"){this.setState({mlocation: value}, console.log("State for ", field, " uppdated to: ", this.state.mlocation))}
-    if (field === "Ingredients"){this.setState({ingredients: value}, console.log("State for ", field, " uppdated to: ", this.state.ingredients))}
-    if (field === "Labels"){this.setState({labels: value}, console.log("State for ", field, " uppdated to: ", this.state.labels))}
-  }
-  // when component mounts, first thing it does is fetch all existing data in our db
-  // then we incorporate a polling logic so that we can easily see if our db has
-  // changed and implement those changes into our UI
-  componentDidMount() {
-    //this.getDataFromDb();
-    if (!this.state.intervalIsSet) {
-    //  let interval = setInterval(this.getDataFromDb, 1000000);
-      //this.setState({ intervalIsSet: interval });
-    }
-  }
-
-  // never let a process live forever
-  // always kill a process everytime we are done using it
-  componentWillUnmount() {
-    if (this.state.intervalIsSet) {
-      clearInterval(this.state.intervalIsSet);
-      this.setState({ intervalIsSet: null });
-    }
-  }
-
-  // just a note, here, in the front end, we use the id key of our data object
-  // in order to identify which we want to Update or delete.
-  // for our back end, we use the object id assigned by MongoDB to modify
-  // data base entries
-
-  // our first get method that uses our backend api to
-  // fetch data from our data base
-  getDataFromDb = (query) => {
-    let url = "http://localhost:27017/api/getData" + query
-    console.log("getDataFromDb Fired with URL: ", url)
-    fetch(url) //  Query format here: fetch("http://localhost:27017/api/getData" + "?id=234234324342&brands=Taste%20Adventure")"?id=0000000035590&brands=Taste%20Adventure"
-    //.then(data => data.text()).then(data => console.log(data))
-    .then(data => data.json())
-    .then(answer => this.setState({ data: answer }, console.log(this.state.data)));
-  };
-
-  // our put method that uses our backend api
-  // to create new query into our data base
-  putDataToDB = message => {
-    let currentIds = this.state.data.map(data => data.id);
-    let idToBeAdded = 0;
-    while (currentIds.includes(idToBeAdded)) {
-      ++idToBeAdded;
-    }
-    axios.post("http://localhost:27017/api/putData", {
-      id: idToBeAdded,
-      message: message
-    });
-  };
-  // our delete method that uses our backend api
-  // to remove existing database information
-  deleteFromDB = idTodelete => {
-    let objIdToDelete = null;
-    this.state.data.forEach(dat => {
-      if (dat.id == idTodelete) {
-        objIdToDelete = dat._id;
-      }
-    });
-    axios.delete("http://localhost:27017/api/deleteData", {
-      data: {
-        id: objIdToDelete
-      }
-    });
-  };
-  // our update method that uses our backend api
-  // to overwrite existing data base information
-  updateDB = (idToUpdate, updateToApply) => {
-    let objIdToUpdate = null;
-    this.state.data.forEach(dat => {
-      if (dat.id == idToUpdate) {
-        objIdToUpdate = dat._id;
-      }
-    });
-
-    axios.post("http://localhost:27017/api/updateData", {
-      id: objIdToUpdate,
-      update: { message: updateToApply }
-    });
-  };
-
+class App extends React.Component {
   render() {
    return (
       <div>
@@ -129,11 +14,10 @@ class MainApp extends React.Component {
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <div className="container-fluid">
-          {this.state.data[0] ? this.state.data[0].manufacturing_places: "places here"}
           <div className="row">
             <div className="col-md-4">
               <h3 className="text-center">
-                Nosh
+                Open Food Facts Data Query
               </h3>
               <form className="form-horizontal">
                 <fieldset>
@@ -141,19 +25,19 @@ class MainApp extends React.Component {
                   <div className="form-group">
                     <label className="col-md-12 control-label" htmlFor="Universal Product Code">Universal Product Code</label>
                     <div className="col-md-12">
-                      <input onChange={(e)=>this.handleChange(e)} className="form-control input-md" id="Universal Product Code" name="Universal Product Code" placeholder="(GTIN-12)" type="text" />
+                      <input className="form-control input-md" id="Universal Product Code" name="Universal Product Code" placeholder="(GTIN-12)" type="text" />
                     </div>
                   </div>{/* Search input*/}
                   <div className="form-group">
                     <label className="col-md-12 control-label" htmlFor="Manufacturing Location">Manufacturing Location</label>
                     <div className="col-md-12">
-                      <input onChange={(e)=>this.handleChange(e)} className="form-control input-md" id="Manufacturing Location" name="Manufacturing Location" placeholder="Address" type="search" />
+                      <input className="form-control input-md" id="Manufacturing Location" name="Manufacturing Location" placeholder="Address" type="search" />
                     </div>
                   </div>{/* Select Multiple */}
                   <div className="form-group">
                     <label className="col-md-12 control-label" htmlFor="Ingredients">Ingredients</label>
                     <div className="col-md-12">
-                      <select onChange={(e)=>this.handleChange(e)} className="form-control" id="Ingredients" multiple="multiple" name="Ingredients">
+                      <select className="form-control" id="Ingredients" multiple="multiple" name="Ingredients">
                         <option value="soy">
                           Soy
                         </option>
@@ -170,20 +54,20 @@ class MainApp extends React.Component {
                     <label className="col-md-12 control-label" htmlFor="Labels">Labels</label>
                     <div className="col-md-12">
                       <div className="checkbox">
-                        <label onChange={(e)=>this.handleChange(e)} htmlFor="Labels-0"><input id="Labels-0" name="Labels" type="checkbox" defaultValue="organic" /> Certified Organic</label>
+                        <label htmlFor="Labels-0"><input id="Labels-0" name="Labels" type="checkbox" defaultValue="organic" /> Certified Organic</label>
                       </div>
                       <div className="checkbox">
-                        <label onChange={(e)=>this.handleChange(e)} htmlFor="Labels-1"><input id="Labels-1" name="Labels" type="checkbox" defaultValue="etc" /> etc.</label>
+                        <label htmlFor="Labels-1"><input id="Labels-1" name="Labels" type="checkbox" defaultValue="etc" /> etc.</label>
                       </div>
                       <div className="checkbox">
-                        <label  onChange={(e)=>this.handleChange(e)} htmlFor="Labels-2"><input id="Labels-2" name="Labels" type="checkbox" defaultValue="etc" /> etc.</label>
+                        <label htmlFor="Labels-2"><input id="Labels-2" name="Labels" type="checkbox" defaultValue="etc" /> etc.</label>
                       </div>
                     </div>
                   </div>
                   <div className="form-group">
                     <label className="col-md-12 control-label" htmlFor="Submit" />
                     <div className="col-md-12">
-                      <button className="btn btn-primary" id="Submit" name="Submit" onClick={(e)=>this.handleSubmit(e)}>Submit</button> <button className="btn btn-warning" id="Clear" name="Clear">Clear</button>
+                      <button className="btn btn-primary" id="Submit" name="Submit">Submit</button> <button className="btn btn-warning" id="Clear" name="Clear">Clear</button>
                     </div>
                   </div>
                 </fieldset>
@@ -199,4 +83,4 @@ class MainApp extends React.Component {
   }
 };
 
-export default MainApp;
+export default App;
