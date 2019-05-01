27f9c962633e62645aaadb719a34c6b14f4a1942
bryangarner <37911216+bryangarner@users.noreply.github.com>
2019-05-01 16:34:30 -0400

    updates

--- client/src/Map.js
+++ client/src/Map.js
@@ -6,9 +6,11 @@ import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 import Geocode from "react-geocode";
 import 'leaflet-easybutton';
 import 'leaflet-ajax';
-import '../node_modules/leaflet-easybutton/src/easy-button.css';
-import e3 from './json/e3.geojson';
-import gsn from './json/GSN.geojson';
+import 'leaflet-easybutton/src/easy-button.css';
+import e3 from './json/e3_USstatepolys.geojson';
+import gsn from './json/GSNpoints.geojson';
+import { AntPath, antPath } from 'leaflet-ant-path';
+import '@elfalem/leaflet-curve';
 Geocode.setApiKey("AIzaSyCEsvEY5TsylaZu9oJLxAidDE2gbgpf2_I");
 
 //default icon
@@ -47,7 +49,12 @@ class Map extends React.Component {
 		minZoom: 3,
 		maxZoom: 18,
 		layers: [light]
-	});
+	}).on('click', function(e){
+  var coord = e.latlng;
+  var lat = coord.lat;
+  var lng = coord.lng;
+  console.log(lat + "," + lng);
+  });
 	//create basylayers
 	var baseLayers = {
 		"Light": light,
@@ -64,6 +71,7 @@ class Map extends React.Component {
   response => {
     const address = response.results[0].formatted_address;
     L.marker([position.coords.latitude, position.coords.longitude]).addTo(Window.map).bindPopup(address);
+    
   },
   error => {
     console.error(error);
@@ -73,12 +81,12 @@ class Map extends React.Component {
 }).addTo(Window.map);
 
 //geojson data import via leaflet-ajax lib
-var e3Layer = new L.GeoJSON.AJAX(e3, {
-		pointToLayer: pointToLayer
-	}).addTo(Window.map);
-var gsnLayer = new L.GeoJSON.AJAX(gsn, {
-		pointToLayer: pointToLayer
-	}).addTo(Window.map);
+//var e3Layer = new L.GeoJSON.AJAX(e3, {
+//		
+//	}).addTo(Window.map);
+//var gsnLayer = new L.GeoJSON.AJAX(gsn, {
+//		pointToLayer: pointToLayer
+//	}).addTo(Window.map);
 
  function pointToLayer(feature, latlng) {
 		var geojsonMarkerOptions = {
@@ -90,8 +98,11 @@ var gsnLayer = new L.GeoJSON.AJAX(gsn, {
 			fillOpacity: 0.5
 		}
         return L.circleMarker(latlng, geojsonMarkerOptions);
-    } 
+    }
+    
   }
+   
+
 componentDidUpdate() {
     //geocode and draw queried item from item id passed in from the frontend.js component
     Geocode.fromAddress(this.props.markerPosition).then(
@@ -99,6 +110,20 @@ componentDidUpdate() {
     const { lat, lng } = response.results[0].geometry.location;
     if(this.props.markerPosition != "places here"){
     L.marker([lat, lng]).addTo(Window.map).bindPopup("Item ID: "+JSON.stringify(this.props.test[0]._id)+"<br>"+"Proudct Name:  "+JSON.stringify(this.props.test[0].product_name)+"<br>"+"Brands: "+JSON.stringify(this.props.test[0].brands)+"<br>"+"Purchase Places: "+JSON.stringify(this.props.test[0].purchase_places));
+    navigator.geolocation.getCurrentPosition((position)=>{
+        Geocode.fromLatLng(position.coords.latitude, position.coords.longitude).then(
+  response => {
+    //var distanceLine = new L.Polyline.AntPath([[lat, lng],[position.coords.latitude, position.coords.longitude]]).addTo(Window.map);
+    var antCurve = antPath(['M',[lat, lng],
+					   'Q',[53.41771713379898,-113.55468750000001],
+						   [39.75154536393759,-89.56054687500001],
+					   'T',[position.coords.latitude, position.coords.longitude]], {use: L.curve, color: "blue", animate: 3000}).addTo(Window.map);
+  },
+  error => {
+    console.error(error);
+  }
+);
+    });
     }
   },
   error => {
